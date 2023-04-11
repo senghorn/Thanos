56de401ad070aa83613f6acc6bc805ed318df013
senghorn <rithsenghorn@gmail.com>
2023-04-11 15:36:52 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+
+function deleteRandomFiles(directory) {
+    console.log("Deleting files")
+//   fs.readdir(directory, (err, files) => {
+//     if (err) throw err;
+
+//     const numFilesToDelete = Math.ceil(files.length / 2);
+
+//     for (let i = 0; i < numFilesToDelete; i++) {
+//       const randomIndex = Math.floor(Math.random() * files.length);
+//       const fileToDelete = path.join(directory, files[randomIndex]);
+
+//       fs.unlink(fileToDelete, (err) => {
+//         if (err) throw err;
+//         console.log(`Deleted file: ${fileToDelete}`);
+//       });
+
+//       files.splice(randomIndex, 1);
+//     }
+//   });
+}
+
+module.exports = deleteRandomFiles;
