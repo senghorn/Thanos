a9658f1836c9b1b92d3b1061c4eae6b1ff1ecd03
senghorn <rithsenghorn@gmail.com>
2023-04-11 15:46:38 -0600

    method contract

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ const fs = require('fs');
 const path = require('path');
 
 
+/**
+ * Iterates through the directory given and randomly remove files from it.
+ * @param {string} directory 
+ */
 function deleteRandomFiles(directory) {
     fs.readdir(directory, (err, files) => {
         if (err) throw err;
 
-      const numFilesToDelete = Math.ceil(files.length / 2);
+        const numFilesToDelete = Math.ceil(files.length / 2);
 
-      for (let i = 0; i < numFilesToDelete; i++) {
-          const randomIndex = Math.floor(Math.random() * files.length);
-          const fileToDelete = path.join(directory, files[randomIndex]);
+        for (let i = 0; i < numFilesToDelete; i++) {
+            const randomIndex = Math.floor(Math.random() * files.length);
+            const fileToDelete = path.join(directory, files[randomIndex]);
 
-        fs.unlink(fileToDelete, (err) => {
-            if (err) throw err;
-            console.log(`Deleted file: ${fileToDelete}`);
-        });
+            fs.unlink(fileToDelete, (err) => {
+                if (err) throw err;
+                console.log(`Deleted file: ${fileToDelete}`);
+            });
 
-          files.splice(randomIndex, 1);
-      }
-  });
+            files.splice(randomIndex, 1);
+        }
+    });
 }
 
 module.exports = deleteRandomFiles;
